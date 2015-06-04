20c3c3c6c1118123ab1fba4e656130810d564811
David Desmaisons <desmaisons_david@hotmail.com>
2015-06-04 13:23:53 -0300

    Fix layout issue

--- dist/bootstrap-tokenfield.js
+++ dist/bootstrap-tokenfield.js
@@ -854,7 +854,7 @@
 
       this.preventCreateTokens = true
 
-      this.$input.val( attrs.value )
+      this.$input.val( attrs.label )
                 .select()
                 .data( 'edit', true )
                 .width( tokenWidth )
@@ -886,7 +886,7 @@
     }
 
   , remove: function (e, direction) {
-      if (this.$input.is(document.activeElement) || this._disabled || this._readonly) return
+      if (this._disabled || this._readonly) return
 
       var $token = (e.type === 'click') ? $(e.target).closest('.token') : this.$wrapper.find('.token.active')
 
